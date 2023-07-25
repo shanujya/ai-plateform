b51139d1bdd6e9e446e676aff8ce0f31f74009cf
Shanujya Mishra <shanujyamishrajan94@gmail.com>
2023-07-25 21:28:17 +0530

    set api limit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { Montserrat } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { FreeCounter } from "@/components/free-counter";
 
 const monsterrat = Montserrat({
   weight: "600",
@@ -57,7 +58,13 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps{
+  apiLimitCount: number;
+}
+
+const Sidebar = ({
+  apiLimitCount=0
+}: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -85,6 +92,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount}/>
     </div>
   );
 };
